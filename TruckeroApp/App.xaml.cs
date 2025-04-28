37402c3ffdf5615288c3f3fd47c8320fa05e8a30
Guillermo Jiménez <lg061870@gmail.com>
2025-04-28 00:27:28 -0600

    🧹 Cleanup: Remove old TruckeroApp, add new clean MAUI/Blazor TruckeroApp project

--- TruckeroApp/App.xaml.cs
+++ TruckeroApp/App.xaml.cs
@@ -1,26 +1,14 @@
-﻿using Microsoft.Extensions.Logging;
-using Microsoft.Maui;
+﻿namespace TruckeroApp;
 
-namespace TruckeroApp
+public partial class App : Application
 {
-    public partial class App : Application
-    {
-        public App(IServiceProvider serviceProvider)
-        {
-            InitializeComponent();
-        }
+	public App()
+	{
+		InitializeComponent();
+	}
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            var services = IPlatformApplication.Current?.Services;
-            if (services == null)
-            {
-                throw new InvalidOperationException("Services are not available.");
-            }
-
-            var logger = services.GetRequiredService<ILogger<MainPage>>();
-            var mainPage = new MainPage(logger);
-            return new Window(mainPage) { Title = "TruckeroApp" };
-        }
-    }
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "TruckeroApp" };
+	}
 }
